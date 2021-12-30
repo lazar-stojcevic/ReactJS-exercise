c51d5d32f193679d6644c9c935c409c8c34dceca
Bojan <lupulov.ra77.2018@uns.ac.rs>
2021-12-30 15:55:35 +0100

    dodavanje prikaza svih vikendica kod admina

--- front/src/components/Admin/InnerAdminComponents/AllBoats.vue
+++ front/src/components/Admin/InnerAdminComponents/AllBoats.vue
@@ -1,9 +1,42 @@
 <template>
-<p>3.7. Profil broda/čamca, prema tome bice odradjeno nakon ove kontrolne tacke</p>
+  <div>
+    <h3><strong>BOATS</strong></h3>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <td>NAME</td>
+        <td>FULL ADDRESS</td>
+        <td>RATING</td>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="boat in boats" :key="boat.id">
+        <td>{{boat.name}}</td>
+        <td>{{boat.address.street}} {{boat.address.city}} {{boat.address.country}}</td>
+        <td>{{boat.rating}}</td>
+        <td><button class="btn btn-danger">DELETE</button></td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      boats: []
+    }
+  },
+
+  mounted() {
+  },
+
+  methods:{
+    loadAllBoats(){
+
+    }
+  }
 }
 </script>
 
--- front/src/components/Admin/InnerAdminComponents/AllCottages.vue
+++ front/src/components/Admin/InnerAdminComponents/AllCottages.vue
@@ -14,7 +14,7 @@
         <td>{{cottage.name}}</td>
         <td>{{cottage.address.street}} {{cottage.address.city}} {{cottage.address.country}}</td>
         <td>{{cottage.rating}}</td>
-        <td><button class="btn btn-danger">DELETE</button></td>
+        <td><button class="btn btn-danger" @click="deleteCottage(cottage.id)">DELETE</button></td>
       </tr>
       </tbody>
     </table>
@@ -31,12 +31,20 @@ export default {
     }
   },
   mounted() {
-    CottageService.getAllCottages().then(res => {this.cottages = res.data;}).catch(() => {
-      alert("THERE IS SOME PROBLEM WITH LOADING ALL COTAGES");
-    });
+    this.loadAllCottage();
   },
   methods:{
+    loadAllCottage(){
+      CottageService.getAllCottages().then(res => {this.cottages = res.data;}).catch(() => {
+        alert("THERE IS SOME PROBLEM WITH LOADING ALL COTAGES");
+      });
+    },
 
+    deleteCottage(id) {
+      CottageService.delete(id).then(() => {this.loadAllCottage()}).catch(() => {
+        alert("THERE IS SOME PROBLEM WITH DELETING COTTAGE");
+      })
+    }
   }
 }
 </script>
