1bd5464d0ce6706319985cc04db1d57d61a722c8
Bojan <lupulov.ra77.2018@uns.ac.rs>
2022-01-01 22:07:31 +0100

    Dodavanje prikaza informacija o korisniku u proslim rezervacijama

--- front/src/components/FishingInstructor/HistoryOfReservations.vue
+++ front/src/components/FishingInstructor/HistoryOfReservations.vue
@@ -1,22 +1,41 @@
 <template>
 <div class="container">
+  <!--MODAL-->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">CUSTOMER PROFILE</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <table class="table-bordered">
+            <tr><td>FIRSTNAME:</td><td/><td><strong>{{customer.firstname}}</strong></td></tr>
+            <tr><td>LASTNAME:</td><td/><td><strong>{{customer.lastName}}</strong></td></tr>
+            <tr><td>EMAIL:</td><td/><td><strong>{{customer.email}}</strong></td></tr>
+            <tr><td>ADDRESS:</td><td/><td><strong>{{customer.address.street}}, {{customer.address.city}}, {{customer.address.country}}</strong></td></tr>
+            <tr><td>PHONE:</td><td/><td><strong>{{customer.phone}}</strong></td></tr>
+            <tr><td>PENALTY POINTS:</td><td/><td><strong>{{customer.penaltyPoints}}</strong></td></tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
   <div style="margin-top: 15px">
     <table class="table table-striped">
       <thead>
       <tr>
         <td>START TIME</td>
         <td>PRICE</td>
-        <td>CUSTOMER</td>
-        <td>REPORT</td>
+        <td>CUSTOMER MAIL</td>
       </tr>
       </thead>
       <tbody>
-        <tr v-for="pr in pastReservations" :key="pr.id">
+        <tr v-for="(pr, index) in pastReservations" :key="pr.id" class="showCustomer" @click="showCustomer(index)"
+            data-bs-toggle="modal" data-bs-target="#exampleModal">
           <td class="col-md-2">{{pr.reservationStart}}</td>
           <td class="col-md-1">{{pr.price}}</td>
-          <td class="col-md-2">USERNAME</td>
-          <td v-if="pr.report !== null">{{pr.report}}</td>
-          <td v-else><button class="btn btn-outline-success col-md-12">ADD REPORT</button></td>
+          <td class="col-md-2">{{pr.customer.email}}</td>
         </tr>
       </tbody>
     </table>
@@ -30,10 +49,9 @@ import LogInService from "@/Services/LogInService";
 export default {
   data(){
     return{
-      pastReservations:{
-        report: ''
-      },
-      mode: 'neutral'
+      pastReservations:[],
+      mode: 'neutral',
+      customer: {firstname: '', address: {street: ''}}
     }
   },
   mounted() {
@@ -42,11 +60,17 @@ export default {
     }).catch(() => {alert("THERE IS SOME ERROR WITH LOADING PAST RESERVATIONS")});
   },
   methods:{
-
+    showCustomer(index){
+      this.customer = this.pastReservations[index].customer;
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.showCustomer:hover{
+  background-color: antiquewhite;
+  font-weight: bolder;
+  cursor: pointer;
+}
 </style>
