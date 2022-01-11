ccfbae0e2ab0f24abdecff2b9d7fae0f04434ac0
lazar-stojcevic <stojcevic.ra84.2018@uns.ac.rs>
2022-01-12 00:11:32 +0100

    Pomeranje kalendara na prikazu profila broda

--- front/src/components/Customer/ShowOne/OneBoat.vue
+++ front/src/components/Customer/ShowOne/OneBoat.vue
@@ -1,34 +1,38 @@
 <template>
   <div style="margin-left: 10px">
-    <b-button v-if="this.subcribed && LogInService.getRole() === 'ROLE_CUSTOMER'" style="margin-left: 20px; margin-top: 10px" variant="danger" v-on:click="unsubscribe">Unsubscribe</b-button>
-    <b-button v-else-if="LogInService.getRole() === 'ROLE_CUSTOMER'" style="margin-left: 20px; margin-top: 10px" variant="success" v-on:click="subscribe">Subscribe</b-button>
-    <br>
-    <h3 style="color: aqua">Name: {{boat.name}}</h3>
-    <br>
-    <h4 style="color:cornflowerblue">Type: {{boat.type}} </h4>
-    <br>
-    <h5 style="color:cornflowerblue">Number of engines: {{boat.numberOfEngines}} </h5>
-    <br>
-    <h5 style="color:cornflowerblue">Top speed: {{boat.topSpeed}} km/h </h5>
-    <br>
-    <h5 style="color:cornflowerblue">Description: {{boat.promo}}. </h5>
-    <br>
-    <h5 style="color:cornflowerblue">Rules: {{boat.conductRules}}. </h5>
-    <br>
-    <h5 style="color:cornflowerblue">Max capacity: {{boat.capacity}} </h5>
-    <br>
-    <h5 style="color:cornflowerblue">Address: {{boat.address.country}}, {{boat.address.city}}, {{boat.address.street}} </h5>
-    <br>
-    <br>
-    <h3 style="color:green">Price per day: {{boat.priceList.price}} </h3>
-    <br>
-    <h3 style="color:cornflowerblue"> Additional services:</h3>
-    <div>
-      <h4 style="margin-left: 15px; color: chartreuse" v-for="service in this.services " :key="service.id"> {{ service.name }} - {{service.addPrice}}</h4>
-    </div>
-    <router-link class="btn btn-success" :to='"/boatActions/" + this.id'>ACTIONS</router-link>
-    <div style="margin-top: 20px; background-color: beige">
-      <FullCalendar :options="calendarOptions"/>
+    <div style="display: flex">
+      <div>
+        <b-button v-if="this.subcribed && LogInService.getRole() === 'ROLE_CUSTOMER'" style="margin-left: 20px; margin-top: 10px" variant="danger" v-on:click="unsubscribe">Unsubscribe</b-button>
+        <b-button v-else-if="LogInService.getRole() === 'ROLE_CUSTOMER'" style="margin-left: 20px; margin-top: 10px" variant="success" v-on:click="subscribe">Subscribe</b-button>
+        <br>
+        <h3 style="color: aqua">Name: {{boat.name}}</h3>
+        <br>
+        <h4 style="color:cornflowerblue">Type: {{boat.type}} </h4>
+        <br>
+        <h5 style="color:cornflowerblue">Number of engines: {{boat.numberOfEngines}} </h5>
+        <br>
+        <h5 style="color:cornflowerblue">Top speed: {{boat.topSpeed}} km/h </h5>
+        <br>
+        <h5 style="color:cornflowerblue">Description: {{boat.promo}}. </h5>
+        <br>
+        <h5 style="color:cornflowerblue">Rules: {{boat.conductRules}}. </h5>
+        <br>
+        <h5 style="color:cornflowerblue">Max capacity: {{boat.capacity}} </h5>
+        <br>
+        <h5 style="color:cornflowerblue">Address: {{boat.address.country}}, {{boat.address.city}}, {{boat.address.street}} </h5>
+        <br>
+        <br>
+        <h3 style="color:green">Price per day: {{boat.priceList.price}} </h3>
+        <br>
+        <h3 style="color:cornflowerblue"> Additional services:</h3>
+        <div>
+          <h4 style="margin-left: 15px; color: chartreuse" v-for="service in this.services " :key="service.id"> {{ service.name }} - {{service.addPrice}}</h4>
+        </div>
+        <router-link class="btn btn-success" :to='"/boatActions/" + this.id'>ACTIONS</router-link>
+      </div>
+      <div style="margin-top: 20px; background-color: beige; width: 50vw; height: 60vh; margin-left: 50px">
+        <FullCalendar :options="calendarOptions"/>
+      </div>
     </div>
   </div>
 </template>
