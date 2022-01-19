909946f6144893dee35cb19ace0723f587faeeea
lazar-stojcevic <stojcevic.ra84.2018@uns.ac.rs>
2022-01-19 17:47:46 +0100

    Bozo pomozi 2

--- front/server.js
+++ front/server.js
@@ -3,9 +3,9 @@ const serveStatic = require('serve-static')
 const path = require('path')
 const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use('/', serveStatic(path.join(__dirname, '/dist/')));
 app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 })
 
 const port = process.env.PORT || 8080
