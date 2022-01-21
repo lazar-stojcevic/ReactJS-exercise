0b9eb4eb8c6a8851e1bca1f47e348865279de32f
lazar-stojcevic <stojcevic.ra84.2018@uns.ac.rs>
2022-01-21 15:28:26 +0100

    Dodatna 2 unit testa za Student 1

--- backend/src/test/java/com/example/backend/service/AdventureReservationServiceTests.java
+++ backend/src/test/java/com/example/backend/service/AdventureReservationServiceTests.java
@@ -1,9 +1,6 @@
 package com.example.backend.service;
 
-import com.example.backend.Beans.Address;
-import com.example.backend.Beans.Adventure;
-import com.example.backend.Beans.AdventureReservation;
-import com.example.backend.Beans.Customer;
+import com.example.backend.Beans.*;
 import com.example.backend.Dtos.ReservationSearchDto;
 import com.example.backend.Repository.AdventureReservationRepository;
 import com.example.backend.Services.AdventureReservationService;
@@ -20,6 +17,7 @@ import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 @SpringBootTest
 public class AdventureReservationServiceTests {
@@ -41,6 +39,7 @@ public class AdventureReservationServiceTests {
     // Student 1
     @Test
     public void testGetAllAvailableReservationsForSearch() {
+        //given
         AdventureReservation reservation1 = new AdventureReservation();
         Address address1 = new Address();
         address1.setCountry("Srbija"); address1.setStreet("ulica 1"); address1.setCity("grad 1");
@@ -77,9 +76,78 @@ public class AdventureReservationServiceTests {
         Customer customer1 = new Customer();
         customer1.setFirstname("Testic");
 
+        //when
         Mockito.when(adventureReservationRepositoryMock.findByreservationStartBetween(any(), any())).thenReturn(retVal);
         Mockito.when(customerServiceMock.findCustomerById(1L)).thenReturn(customer1);
-
+        //then
         assertThat(subject.getAllAvailableReservationsForSearch(searchDto).size()).isEqualTo(2);
     }
+
+    //Student 1
+    @Test
+    public void unsuccessfulCancelReservationTest(){
+        //given
+        AdventureReservation reservation1 = new AdventureReservation();
+        reservation1.setId(1);
+        Address address1 = new Address();
+        address1.setCountry("Srbija"); address1.setStreet("ulica 1"); address1.setCity("grad 1");
+        Adventure adventure1 = new Adventure();
+        adventure1.setMaxPersons(10);
+        adventure1.setAddress(address1);
+        reservation1.setAdventure(adventure1);
+        reservation1.setForbidenCustomers(new ArrayList<>());
+        reservation1.setLastDateToReserve(LocalDateTime.now().plusDays(8));
+        reservation1.setReservationStart(LocalDateTime.now().plusDays(2));
+        //when
+        Mockito.when(adventureReservationRepositoryMock.findById(anyLong())).thenReturn(java.util.Optional.of(reservation1));
+        //then
+        assertThat(subject.cancelAdventureReservation(1)).isNull();
+    }
+
+    //Student 1
+    @Test
+    public void testGetAllNextFreeActionsOfInstructor(){
+        //given
+        FishingInstructor instructor = new FishingInstructor();
+        instructor.setId(10);
+        FishingInstructor instructor2 = new FishingInstructor();
+        instructor2.setId(11);
+
+        AdventureReservation reservation1 = new AdventureReservation();
+        Address address1 = new Address();
+        address1.setCountry("Srbija"); address1.setStreet("ulica 1"); address1.setCity("grad 1");
+        Adventure adventure1 = new Adventure();
+        adventure1.setInstructor(instructor);
+        adventure1.setMaxPersons(10);
+        adventure1.setAddress(address1);
+        reservation1.setAdventure(adventure1);
+        reservation1.setForbidenCustomers(new ArrayList<>());
+        reservation1.setLastDateToReserve(LocalDateTime.now().plusDays(8));
+        reservation1.setReservationStart(LocalDateTime.now().plusDays(15));
+        reservation1.setDiscount(10);
+
+        AdventureReservation reservation2 = new AdventureReservation();
+        Address address2 = new Address();
+        address2.setCountry("Srbija"); address2.setStreet("ulica 2"); address2.setCity("grad 2");
+        Adventure adventure2 = new Adventure();
+        adventure2.setInstructor(instructor2);
+        adventure2.setMaxPersons(15);
+        adventure2.setAddress(address2);
+        reservation2.setAdventure(adventure2);
+        reservation2.setForbidenCustomers(new ArrayList<Customer>());
+        reservation2.setLastDateToReserve(LocalDateTime.now().plusDays(10));
+        reservation2.setReservationStart(LocalDateTime.now().plusDays(21));
+        reservation2.setDiscount(10);
+
+        ArrayList<AdventureReservation> retVal = new ArrayList<>();
+        retVal.add(reservation1);
+        retVal.add(reservation2);
+        //when
+        Mockito.when(adventureReservationRepositoryMock.findAll()).thenReturn(retVal);
+        //then
+        assertThat(subject.getAllNextFreeActionsOfInstructor(10).size()).isEqualTo(1);
+
+    }
+
+
 }
