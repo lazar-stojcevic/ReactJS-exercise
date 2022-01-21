c6b82401284060a228a36b97d70d7571162c82d5
Bojan <lupulov.ra77.2018@uns.ac.rs>
2022-01-21 13:54:52 +0100

    Dodavanje skripte

---
+++ backend/src/test/resources/test-data.sql
@@ -0,0 +1,8 @@
+insert into ROLE (id, name) values (1, 'ROLE_CUSTOMER');
+insert into ROLE (id, name) values (2, 'ROLE_ADMIN');
+insert into ROLE (id, name) values (3, 'ROLE_FISHING_INSTRUCTOR');
+insert into ROLE (id, name) values (4, 'ROLE_COTTAGE_OWNER');
+insert into ROLE (id, name) values (5, 'ROLE_BOAT_OWNER');
+
+
+
