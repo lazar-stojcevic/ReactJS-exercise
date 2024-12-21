3ca2e42eda81a3e170aa7a76e374ac2e4bb2624b
lazar.stojcevic <stojcevic00@gmail.com>
2024-12-21 19:25:37 +0100

    Navigation bar preview

--- react-front/frontend-react-version/src/App.tsx
+++ react-front/frontend-react-version/src/App.tsx
@@ -3,27 +3,66 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Context from "./pages/Context";
 import Login from "./pages/LoginPage/Login";
+import { AppBar, Box, Link, Toolbar, Typography } from "@mui/material";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/login">Login</a></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Context />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <AppBar position="fixed" sx={{ width: "100vw" }}>
+        <Toolbar sx={{ justifyContent: "space-around", padding: 0 }}>
+          <Typography variant="h6" component="div">
+            <Link
+              href="/"
+              color="inherit"
+              underline="none"
+              sx={{ fontWeight: "bold" }}
+            >
+              Home
+            </Link>
+          </Typography>
+          <Typography variant="h6" component="div">
+            <Link
+              href="/about"
+              color="inherit"
+              underline="none"
+              sx={{ fontWeight: "bold" }}
+            >
+              About
+            </Link>
+          </Typography>
+          <Typography variant="h6" component="div">
+            <Link
+              href="/contact"
+              color="inherit"
+              underline="none"
+              sx={{ fontWeight: "bold" }}
+            >
+              Contact
+            </Link>
+          </Typography>
+          <Typography variant="h6" component="div">
+            <Link
+              href="/login"
+              color="inherit"
+              underline="none"
+              sx={{ fontWeight: "bold" }}
+            >
+              Login
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <BrowserRouter>
+        <Box>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Context />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </div>
   );
 }
 
